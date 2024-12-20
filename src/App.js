ef2f6028f330f148bc397a8b0541028d913684eb
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2024-12-20 15:45:03 +0100

    dia 2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 
 function App() {
   return (
     <div className="App">
 
-      <TodoItems/>
-      <TodoItems/>
-      <TodoItems/>
+      <TodoCounter/>
+      <TodoSearch/>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
+      
+      {<CreateTodoButton/>}
     </div>
-  );
-}
-
-function TodoItems(){
-  return(
-    <li>
-      <span>V</span>
-      <p>Llorar con la Llorona</p>
-      <span>x</span>
-    </li>
   );
 }
 
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem(){
+    return(
+      <li>
+        <span>V</span>
+        <p>Llorar con la Llorona</p>
+        <span>x</span>
+      </li>
+    );
+  }
+
+export {TodoItem}
