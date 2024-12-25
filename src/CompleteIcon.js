17dd7673e6ef0aa0599620463ccaf5a05f473c76
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2024-12-25 15:13:33 +0100

    dia 10

---
+++ src/CompleteIcon.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {TodoIcon} from './TodoIcon'
+
+function CompleteIcon({completed, onComplete}){
+    return (
+        <TodoIcon type="check" color={completed ? 'green' : 'gray'} onClick={onComplete}/>
+    );
+}
+export {CompleteIcon}
